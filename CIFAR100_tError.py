0389b08c6a79a5d649327a019fdaf9c5f8cc9a9b
mdn33 <54626709+mdn33@users.noreply.github.com>
2020-05-16 18:20:54 +0200

    Update CIFAR100_tError.py
    Fixed indentation

--- CIFAR100_tError.py
+++ CIFAR100_tError.py
@@ -5,9 +5,9 @@ import numpy as np
 import pickle
 import torch
 
+from torchvision.datasets import VisionDataset
 
 
-# from .vision import VisionDataset
 # from .utils import check_integrity, download_and_extract_archive
 from torchvision.datasets import CIFAR100
 
@@ -17,9 +17,11 @@ def pil_loader(f):
     return img.convert('RGB')
 
 
-class CIFAR100_tError():
-  def __init__(self, root, train=True, transform=None, target_transform=None, download=False, lbls=[])
-    self.prova = CIFAR100(root, train=train, transform=None, target_transform=None, download=download)
+class CIFAR100_tError(CIFAR100):
+
+  def __init__(self, root, train=True, transform=None,download=False, lbls=[]):
+    flag = train
+    self.prova = CIFAR100(root, train=train, download=download)
     
     self.data = []
     self.labels = []
@@ -28,25 +30,8 @@ class CIFAR100_tError():
       if label in lbls:
         self.data.append(image)
         self.labels.append(label)
-          
-    def __getitem__(self, index):
-        '''
-        __getitem__ should access an element through its index
-        Args:
-            index (int): Index
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
-        
-        image = pil_loader(self.data[index])
-        label = self.labels[index]
-        
-        if self.transform is not None:
-            image = self.transform(image)
-        
-        return image, label
 
-    def __len__(self):
+  def __len__(self):
         '''
         The __len__ method returns the length of the dataset
         It is mandatory, as this is used by several other components
@@ -54,10 +39,28 @@ class CIFAR100_tError():
         length = len(self.data)
         return length
       
-    def increment(self, newlbls):
-      for element in self.prova:
-        image, label = element
-        if label in newlbls:
-          self.data.append(image)
-          self.labels.append(label)
+          
+  def __getitem__(self, index):
+      '''
+      __getitem__ should access an element through its index
+      Args:
+          index (int): Index
+      Returns:
+          tuple: (sample, target) where target is class_index of the target class.
+      '''
+      
+      image = pil_loader(self.data[index])
+      label = self.labels[index]
+      
+      if self.transform is not None:
+          image = self.transform(image)
+      
+      return image, label
+    
+  def increment(self, newlbls):
+    for element in self.prova:
+      image, label = element
+      if label in newlbls:
+        self.data.append(image)
+        self.labels.append(label)
       
