9afb4e466403b61ec3cdb7714cc40523294dc1ec
Nicolò Cordaro <37053588+Nicordaro@users.noreply.github.com>
2020-05-20 15:39:39 +0200

    Update model_finetuning.py

--- model_finetuning.py
+++ model_finetuning.py
@@ -1,135 +1,95 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "name": "Untitled1.ipynb",
-      "provenance": [],
-      "authorship_tag": "ABX9TyPwVo1nfZKByBPxh0HuCIRb",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Nicordaro/Project_MLDL/blob/master/model_finetuning.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "metadata": {
-        "id": "e4-7RXfOTpoZ",
-        "colab_type": "code",
-        "colab": {}
-      },
-      "source": [
-        "import torch\n",
-        "import torch.nn as nn\n",
-        "import torch.nn.functional as F\n",
-        "\n",
-        "class BasicBlock(nn.Module):\n",
-        "    expansion = 1\n",
-        "\n",
-        "    def __init__(self, in_planes, planes, stride=1):\n",
-        "        super(BasicBlock, self).__init__()\n",
-        "        self.conv1 = nn.Conv2d(\n",
-        "            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)\n",
-        "        self.bn1 = nn.BatchNorm2d(planes)\n",
-        "        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,\n",
-        "                               stride=1, padding=1, bias=False)\n",
-        "        self.bn2 = nn.BatchNorm2d(planes)\n",
-        "\n",
-        "        self.shortcut = nn.Sequential()\n",
-        "        if stride != 1 or in_planes != self.expansion*planes:\n",
-        "            self.shortcut = nn.Sequential(\n",
-        "                nn.Conv2d(in_planes, self.expansion*planes,\n",
-        "                          kernel_size=1, stride=stride, bias=False),\n",
-        "                nn.BatchNorm2d(self.expansion*planes)\n",
-        "            )\n",
-        "\n",
-        "    def forward(self, x):\n",
-        "        out = F.relu(self.bn1(self.conv1(x)))\n",
-        "        out = self.bn2(self.conv2(out))\n",
-        "        out += self.shortcut(x)\n",
-        "        out = F.relu(out)\n",
-        "        return out\n",
-        "\n",
-        "\n",
-        "class Bottleneck(nn.Module):\n",
-        "    expansion = 4\n",
-        "\n",
-        "    def __init__(self, in_planes, planes, stride=1):\n",
-        "        super(Bottleneck, self).__init__()\n",
-        "        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)\n",
-        "        self.bn1 = nn.BatchNorm2d(planes)\n",
-        "        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,\n",
-        "                               stride=stride, padding=1, bias=False)\n",
-        "        self.bn2 = nn.BatchNorm2d(planes)\n",
-        "        self.conv3 = nn.Conv2d(planes, self.expansion *\n",
-        "                               planes, kernel_size=1, bias=False)\n",
-        "        self.bn3 = nn.BatchNorm2d(self.expansion*planes)\n",
-        "\n",
-        "        self.shortcut = nn.Sequential()\n",
-        "        if stride != 1 or in_planes != self.expansion*planes:\n",
-        "            self.shortcut = nn.Sequential(\n",
-        "                nn.Conv2d(in_planes, self.expansion*planes,\n",
-        "                          kernel_size=1, stride=stride, bias=False),\n",
-        "                nn.BatchNorm2d(self.expansion*planes)\n",
-        "            )\n",
-        "\n",
-        "    def forward(self, x):\n",
-        "        out = F.relu(self.bn1(self.conv1(x)))\n",
-        "        out = F.relu(self.bn2(self.conv2(out)))\n",
-        "        out = self.bn3(self.conv3(out))\n",
-        "        out += self.shortcut(x)\n",
-        "        out = F.relu(out)\n",
-        "        return out\n",
-        "\n",
-        "\n",
-        "class ResNet(nn.Module):\n",
-        "    def __init__(self, block, num_blocks, num_classes=10):\n",
-        "        super(ResNet, self).__init__()\n",
-        "        self.in_planes = 64\n",
-        "\n",
-        "        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,\n",
-        "                               stride=1, padding=1, bias=False)\n",
-        "        self.bn1 = nn.BatchNorm2d(64)\n",
-        "        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)\n",
-        "        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)\n",
-        "        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)\n",
-        "        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)\n",
-        "        self.linear = nn.Linear(512*block.expansion, num_classes)\n",
-        "\n",
-        "    def _make_layer(self, block, planes, num_blocks, stride):\n",
-        "        strides = [stride] + [1]*(num_blocks-1)\n",
-        "        layers = []\n",
-        "        for stride in strides:\n",
-        "            layers.append(block(self.in_planes, planes, stride))\n",
-        "            self.in_planes = planes * block.expansion\n",
-        "        return nn.Sequential(*layers)\n",
-        "\n",
-        "    def forward(self, x):\n",
-        "        out = F.relu(self.bn1(self.conv1(x)))\n",
-        "        out = self.layer1(out)\n",
-        "        out = self.layer2(out)\n",
-        "        out = self.layer3(out)\n",
-        "        out = self.layer4(out)\n",
-        "        out = F.avg_pool2d(out, 4)\n",
-        "        out = out.view(out.size(0), -1)\n",
-        "        out = self.linear(out)\n",
-        "        return out\n"
-      ],
-      "execution_count": 0,
-      "outputs": []
-    }
-  ]
-}
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class Bottleneck(nn.Module):
+    expansion = 4
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(Bottleneck, self).__init__()
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.conv3 = nn.Conv2d(planes, self.expansion *
+                               planes, kernel_size=1, bias=False)
+        self.bn3 = nn.BatchNorm2d(self.expansion*planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(ResNet, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
