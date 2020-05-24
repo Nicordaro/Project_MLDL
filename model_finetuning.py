c7d62f32617ac553f8884c303ca2a1bdedbd5ce3
mdn33 <54626709+mdn33@users.noreply.github.com>
2020-05-24 18:48:17 +0200

    Update model_finetuning.py

--- model_finetuning.py
+++ model_finetuning.py
@@ -93,7 +93,6 @@ class ResNet(nn.Module):
         out = F.avg_pool2d(out, 4)
         out = out.view(out.size(0), -1)
         out = torch.nn.functional.normalize(out,p=2,dim=1)
-        out = self.linear(out)
         return out
     
 # We use this function to initialize the net inside the .py
