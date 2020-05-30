4d8439a81bc5f66b0073aaf2110a028426cdde37
mdn33 <54626709+mdn33@users.noreply.github.com>
2020-05-30 08:46:58 +0200

    Update model_finetuning.py
    Functions extract_features and classification implemented

--- model_finetuning.py
+++ model_finetuning.py
@@ -95,6 +95,21 @@ class ResNet(nn.Module):
         out = self.linear(out)
         return out
     
+    def extract_features(self,x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        return out
+    
+    def classification(self,x):
+        out = self.linear(x)
+        return out
+    
+    
 # We use this function to initialize the net inside the .py
 def ResNet18():
     return ResNet(BasicBlock,[2,2,2,2],100)
