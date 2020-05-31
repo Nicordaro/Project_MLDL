5e23e415aff143f96a27864f57be88a4381a17df
Nicolò Cordaro <37053588+Nicordaro@users.noreply.github.com>
2020-05-31 22:12:15 +0200

    Create confusion_matrix.py

---
+++ confusion_matrix.py
@@ -0,0 +1,12 @@
+def update_confusion_matrix(matrix, preds, datas):
+    for pred, data in zip(preds,datas):
+        matrix[data.item(),pred.item()] = matrix[data.item(),pred.item()]+1
+        
+def new_confusion_matrix(lenx=100, leny=100):
+    matrix = np.zeros((leny, lenx))
+    return matrix
+
+def show_confusion_matrix(matrix):
+    fig, ax = plt.subplots(figsize=(15,9))
+    ax = sns.heatmap(matrix, linewidth=0.2,cmap='Reds')
+    plt.show()
