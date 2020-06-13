de67a1174c03d68c6fc743651fe81cb17c12e22a
Nicolò Cordaro <37053588+Nicordaro@users.noreply.github.com>
2020-06-13 10:51:51 +0200

    Update modified_resnet.py

--- point4/modified_resnet.py
+++ point4/modified_resnet.py
@@ -2,6 +2,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.nn
+import math
+from torch.nn.parameter import Parameter
+from torch.nn import Module
 
 
 class BasicBlock(nn.Module):
@@ -109,12 +112,7 @@ class ResNet(nn.Module):
         out = self.linear(x)
         return out
 
-import math
 
-import torch
-from torch.nn.parameter import Parameter
-from torch.nn import functional as F
-from torch.nn import Module
 
 class CosineLinear(Module):
     def __init__(self, in_features, out_features, sigma=True):
@@ -137,28 +135,6 @@ class CosineLinear(Module):
     def forward(self, input):
         out = F.linear(F.normalize(input, p=2,dim=1), \
                 F.normalize(self.weight, p=2, dim=1))
-        if self.sigma is not None:
-            out = self.sigma * out
-        return out
-
-class SplitCosineLinear(Module):
-    #consists of two fc layers and concatenate their outputs
-    def __init__(self, in_features, out_features1, out_features2, sigma=True):
-        super(SplitCosineLinear, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features1 + out_features2
-        self.fc1 = CosineLinear(in_features, out_features1, False)
-        self.fc2 = CosineLinear(in_features, out_features2, False)
-        if sigma:
-            self.sigma = Parameter(torch.Tensor(1))
-            self.sigma.data.fill_(1)
-        else:
-            self.register_parameter('sigma', None)
-
-    def forward(self, x):
-        out1 = self.fc1(x)
-        out2 = self.fc2(x)
-        out = torch.cat((out1, out2), dim=1) #concatenate along the channel
         if self.sigma is not None:
             out = self.sigma * out
         return out
