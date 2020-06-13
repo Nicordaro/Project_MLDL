652b840ec8a43dc4d8494f33e41829edd9aec51d
Alberto Falletta <63041923+albeffe@users.noreply.github.com>
2020-06-13 11:12:47 +0200

    Update modified_resnet.py

--- point4/modified_resnet.py
+++ point4/modified_resnet.py
@@ -9,8 +9,8 @@ from torch.nn import Module
 class CosineLinear(Module):
     def __init__(self, in_features, out_features, sigma=True):
         super(CosineLinear, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
+        self.in_features = in_features  # number of features (512)
+        self.out_features = out_features  # number of classes (100)
         self.weight = Parameter(torch.Tensor(out_features, in_features))
         if sigma:
             self.sigma = Parameter(torch.Tensor(1))
